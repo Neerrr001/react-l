0585451e87066a4aa068dfdc37bfbe960e9752c0
Neerrr001 <neeragarwal711@gmail.com>
2026-03-20 13:30:44 +0530

    LeftContent of page 1 complete

---
+++ ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='p-6'>
+        <h3 className='text-6xl mb-7 font-bold'>Prospective<br /><span>customer</span><br />segmentation</h3>
+        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam architecto, unde dolorem repudiandae eveniet rem molestiae! Error officiis nihil tenetur.</p>
+      </div>
+  )
+}
+
+export default HeroText
