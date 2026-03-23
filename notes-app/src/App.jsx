2f8b409876600ba1d32e7ef2cddcc746362325ec
Neerrr001 <neeragarwal711@gmail.com>
2026-03-23 20:46:00 +0530

    UI done

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log("Form is submitted");  
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen lg:flex bg-black text-white ">
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} className="flex flex-col lg:w-1/2 font-medium gap-5 p-10 ">
+         <h1 className="text-4xl font-bold">Add Notes</h1>
+
+
+        <input type="text" 
+        placeholder="Enter notes heading"
+        className="px-5 py-2 w-full outline-none border-2 rounded"
+        />
+        <textarea type="text" 
+        placeholder="Write details"
+        className="px-5 w-full h-30 py-2 outline-none border-2 rounded"
+        />
+        <button className="bg-white w-full text-black outline-none px-5 py-2 rounded">Add Notes</button>
+      </form>
+
+      <div className="bg-gray-800 lg:border-l lg:w-1/2  p-10">
+        <h1 className="text-4xl font-bold">Recent Notes</h1>
+        <div className='flex gap-5 flex-wrap mt-5 h-full overflow-auto'>
+          <div className="h-52 w-47 rounded-2xl bg-white"></div>
+          <div className="h-52 w-47 rounded-2xl bg-white"></div>
+          <div className="h-52 w-47 rounded-2xl bg-white"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default App
