10a9c74bf5c0b51c2041ef6e15d47ecce5da6b73
Neerrr001 <neeragarwal711@gmail.com>
2026-03-23 20:59:59 +0530

    UI done

--- learn/src/App.jsx
+++ learn/src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+
+  const [title, setTitle] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault()  
+    console.log("Form submitted by", title);
+
+    setTitle('')
+  }
+
   return (
-    <>
-    <div>
-      
-    </div>
-    </>
+    <form onSubmit ={(e)=>{
+      submitHandler(e)
+    }}>
+      <input type="text" placeholder='Enter your name' value={title}
+             onChange={(e)=>{
+              setTitle(e.target.value)
+             }}
+         />
+      <button>Submit</button>
+    </form>
   )
 }
 
