1c51cb4e7b0e8585d473ecda2b942f1b1c30a748
Neerrr001 <neeragarwal711@gmail.com>
2026-03-26 20:00:41 +0530

    Added prev, next btn func

--- gallery-project/src/App.jsx
+++ gallery-project/src/App.jsx
@@ -4,13 +4,14 @@ import axios from 'axios'
 const App = () => {
 
   const [userData, setUserData] = useState([])
+  const [index, setIndex] = useState(1)
 
   useEffect(function(){
     getData()
-  }, [])
+  }, [index])
 
   const getData = async()=>{
-    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=25')
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=20`)
     setUserData(response.data)
   }
 
@@ -21,7 +22,7 @@ const App = () => {
 
       return <div>
         <a href={elem.url} target='_blank'>
-        <div className='h-40 w-44 overflow-hidden rounded-xl'>
+        <div className='h-50 w-60 overflow-hidden rounded-xl'>
           <img className='h-full w-full object-cover'src={elem.download_url} alt="" />
         </div>
         <h2 className='font-medium text-lg '>{elem.author}</h2>
@@ -31,18 +32,31 @@ const App = () => {
   }
 
   return (
-    // <div className='bg-black overflow-auto h-screen p-4 text-white'>
-    //   <button
-    //     onClick={getData}
-    //     className='bg-green-600 active:scale-95 px-5 py-2 mb-4 rounded '>
-    //     Get data
-    //   </button>
-      
-
-      <div className='flex p-4 bg-black text-white flex-wrap gap-5'>
+    <div className='bg-black overflow-y-auto h-screen p-4  text-white'>
+      <h1 className='fixed text-5xl'>{index}</h1>
+      <div className='flex p-4 flex-wrap gap-5'>
         {printUserData}
       </div>
-    // </div>
+
+      <div className='flex mt-4 justify-center items-center p-4 gap-4'>
+        <button
+         className='bg-emerald-400 text-white cursor-pointer active:scale-95 font-medium px-4 py-2 rounded-xl '
+         onClick={()=>{
+          if(index>1){
+          setIndex(index-1)
+          }
+         }}>
+         Prev
+        </button>
+        <button
+         className='bg-emerald-400 text-white cursor-pointer active:scale-95 font-medium  px-4 py-2 rounded-xl '
+         onClick={()=>{
+          setIndex(index+1)
+         }}>
+          Next
+        </button>
+      </div>
+    </div>
   )
 }
 
